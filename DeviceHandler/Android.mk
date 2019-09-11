b084ce968948c6c3c01f7159dac688049b5dc49f
Dan Cartier <nospamdan1@gmail.com>
2019-09-11 04:43:15 -0400

    oppo-common: Use platform certificate/api and update resources linking

--- DeviceHandler/Android.mk
+++ DeviceHandler/Android.mk
@@ -29,18 +29,28 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := DeviceHandler
+LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 LOCAL_USE_AAPT2 := true
+LOCAL_PRIVATE_PLATFORM_APIS := true
 
+package_resource_overlays := $(strip \
+    $(wildcard $(foreach dir, $(PRODUCT_PACKAGE_OVERLAYS), \
+      $(addprefix $(dir)/, packages/apps/DeviceHandler/res))) \
+    $(wildcard $(foreach dir, $(DEVICE_PACKAGE_OVERLAYS), \
+      $(addprefix $(dir)/, packages/apps/DeviceHandler/res))))
+
+LOCAL_RESOURCE_DIR := $(package_resource_overlays) $(LOCAL_RESOURCE_DIR)
 
 LOCAL_PROGUARD_ENABLED := disabled
-LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_DEX_PREOPT := false
+
 LOCAL_JAVA_LIBRARIES := telephony-common
 LOCAL_MODULE_PATH := $(TARGET_OUT)/app
 
 include frameworks/base/packages/SettingsLib/common.mk
 
-
 include $(BUILD_PACKAGE)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
